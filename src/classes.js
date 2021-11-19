6581e6607f5822cfc7fd1899cc1bd960f8cb594b
Dahir Mohamed <dmoabdi98@gmail.com>
2021-11-19 19:00:39 +0000

    created classes for manager , engineer and intern

--- src/classes.js
+++ src/classes.js
@@ -13,3 +13,37 @@ class Employee {
 
   getRole() {}
 }
+
+class Manager extends Employee {
+  constructor(officeNumber) {
+    this.officeNumber;
+
+    super(employeeName, id, email);
+  }
+
+  getRole() {}
+}
+
+class Engineer extends Employee {
+  constructor(github) {
+    this.github;
+
+    super(employeeName, id, email);
+  }
+
+  getGithub() {}
+
+  getRole() {}
+}
+
+class Intern extends Employee {
+  constructor(school) {
+    this.school;
+
+    super(employeeName, id, email);
+  }
+
+  getSchool() {}
+
+  getRole() {}
+}
