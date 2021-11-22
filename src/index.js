b3fe9f04c088e8b74076416211c33e53a2afb948
Dahir Mohamed <dmoabdi98@gmail.com>
2021-11-22 22:07:12 +0000

    tested employee type question in the command line

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 const inquirer = require("inquirer");
-const { starterQuestions, typeOfEmployeeQuestion } = require("./questions");
+const {
+  starterQuestions,
+  typeOfEmployeeQuestion,
+  internQuestions,
+  engineerQuestions,
+} = require("./questions");
+
+const addingAnEmployee = async (typeofEmployee) => {
+  if (typeofEmployee.employeeType === "intern") {
+    const internAnswers = await inquirer.prompt(internQuestions);
+    console.log(internAnswers);
+    return internAnswers;
+  }
+
+  if (typeofEmployee.employeeType === "engineer") {
+    const engineerAnswers = await inquirer.prompt(engineerQuestions);
+    console.log(engineerAnswers);
+    return engineerAnswers;
+  }
+};
 
 const init = async () => {
-  const starterAnswer = await inquirer.prompt(starterQuestions);
-  console.log(starterAnswer);
+  // const starterAnswer = await inquirer.prompt(starterQuestions);
+  // console.log(starterAnswer);
 
   const typeofEmployeeAnswer = await inquirer.prompt(typeOfEmployeeQuestion);
   console.log(typeofEmployeeAnswer);
+
+  addingAnEmployee(typeofEmployeeAnswer);
 };
 
 init();
