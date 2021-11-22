307d9b11ab7caea1c8c8559b84884f81ffa20271
Dahir Mohamed <dmoabdi98@gmail.com>
2021-11-22 22:53:31 +0000

    added a while loop to repeat employee questions

--- src/index.js
+++ src/index.js
@@ -24,16 +24,32 @@ const init = async () => {
   const starterAnswer = await inquirer.prompt(starterQuestions);
   console.log(starterAnswer);
 
-  // question for the type of employee to add to team
-  const typeofEmployeeAnswer = await inquirer.prompt(typeOfEmployeeQuestion);
-  console.log(typeofEmployeeAnswer);
+  // Loop through these set of question to keep adding other employee details
 
-  // questions depending on the type of employee selected
-  addingAnEmployee(typeofEmployeeAnswer);
+  let inProgress = true;
 
-  // question if user want to add another employee
-  const addAnotherAnswer = await inquirer.prompt(addAnotherEmployeeType);
-  console.log(addAnotherAnswer);
+  while (inProgress) {
+    // question for the type of employee to add to team
+    const { employeeType } = await inquirer.prompt(typeOfEmployeeQuestion);
+    console.log(employeeType);
+
+    // questions depending on the type of employee selected
+    if (employeeType === "intern") {
+      const internAnswers = await inquirer.prompt(internQuestions);
+      console.log(internAnswers);
+    }
+
+    if (employeeType === "engineer") {
+      const engineerAnswers = await inquirer.prompt(engineerQuestions);
+      console.log(engineerAnswers);
+    }
+
+    // question if user want to add another employee
+    const { addAnother } = await inquirer.prompt(addAnotherEmployeeType);
+    console.log(addAnother);
+
+    if (!addAnother) inProgress = false;
+  }
 };
 
 init();
