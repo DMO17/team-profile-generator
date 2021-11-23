13bd2cdb6ccabe10d2878550d420da9887dc74f4
Dahir Mohamed <dmoabdi98@gmail.com>
2021-11-23 00:22:07 +0000

    sample results

---
+++ src/sample-results.js
@@ -0,0 +1,15 @@
+const finalResult = {
+  starter: {
+    teamName: "aerg",
+    managerName: "aerg",
+    managerEmployeeID: "aerg",
+    managerEmail: "aerg",
+    managerOfficeNum: "aerg",
+  },
+  employeeDetails: { engineer: [[Engineer], [Engineer]], intern: [[Intern]] },
+};
+
+// const engineerIntern = {
+//     engineer: [ Engineer { name: 'hw', id: 'weqf', email: 'uj', github: 'h5w3' } ],
+//     intern: [ Intern { name: '76j', id: 'qwef', email: '89l', school: 'eht' } ]
+//   }
