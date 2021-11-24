d8cf6a5643d9c99f15a4d1d57d56b5bc85c8af16
Dahir Mohamed <dmoabdi98@gmail.com>
2021-11-24 16:37:51 +0000

    created the whole html page code using classes

--- src/index.js
+++ src/index.js
@@ -73,17 +73,98 @@ const init = async () => {
       "
     >
     
-    ${intern.map((each) => {
-      return each.getRole();
-    })}
+    ${intern
+      .map((each) => {
+        return each.getRole();
+      })
+      .join("")}
 
     </div>
   </section>`;
   };
 
-  console.log(internHtml(intern));
+  const engineerHtml = (engineer) => {
+    return `
+    <section class="container mb-5">
+      <div class="h1 animate__animated animate__fadeInLeftBig">
+        <u>ENGINEER</u>
+      </div>
 
-  const generateHtml = () => {};
+      <div
+        class="
+          d-flex
+          flex-row flex-wrap
+          justify-content-between
+          border border-dark
+          mt-3
+        "
+      >
+
+      
+      ${engineer
+        .map((each) => {
+          return each.getRole();
+        })
+        .join("")}
+      
+      </div>
+    </section>`;
+  };
+
+  const generateHtml = () => {
+    return `<!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>TEAM NAME</title>
+    
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+        />
+    
+        <link
+          rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+          crossorigin="anonymous"
+        />
+    
+        <link rel="stylesheet" href="./style.css" />
+      </head>
+    
+      <body>
+        <main>
+    
+        ${titleAndManagerHtml(manager)}
+
+        ${internHtml(intern)}
+    
+        ${engineerHtml(engineer)}
+    
+          
+        </main>
+    
+        <script
+          src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+          integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+          crossorigin="anonymous"
+        ></script>
+        <script
+          src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+          integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+          crossorigin="anonymous"
+        ></script>
+        <script
+          src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+          integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+          crossorigin="anonymous"
+        ></script>
+      </body>
+    </html>`;
+  };
 };
 
 init();
--- src/lib/Engineer.js
+++ src/lib/Engineer.js
@@ -9,7 +9,28 @@ class Engineer extends Employee {
 
   getGithub() {}
 
-  getRole() {}
+  getRole() {
+    return `  <div
+    class="card animate__animated animate__zoomIn"
+    style="width: 18rem"
+  >
+    <img
+      class="card-img-top"
+      src="./assets/images/engineer.PNG"
+      alt="Card image cap"
+    />
+
+    <div class="card-body">
+      <h3 class="card-title">${this.name}</h3>
+
+      <ul class="list-group">
+        <li class="list-group-item">Employee ID: ${this.id}</li>
+        <li class="list-group-item">Email Address:${this.email}</li>
+        <li class="list-group-item">GitHub Username: ${this.github}</li>
+      </ul>
+    </div>
+  </div>`;
+  }
 }
 
 module.exports = Engineer;
